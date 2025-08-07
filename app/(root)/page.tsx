0670e2899825da637d497090aefc3ad8406216b7
Chris Davis <chris.davis5440@gmail.com>
2025-08-07 15:26:57 -0400

    Setup serverless DB with neon and drizzle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,24 @@
 import BookList from '@/components/BookList';
 import BookOverview from '@/components/BookOverview';
 import { sampleBooks } from '@/constants';
+import { db } from '@/database/drizzle';
+import { users } from '@/database/schema';
 
-const Home = () => (
-  <div>
-    <BookOverview {...sampleBooks[0]} />
+const Home = async () => {
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result));
 
-    <BookList
-      title='Latest Books'
-      books={sampleBooks}
-      containerClassName='mt-28'
-    />
-  </div>
-);
+  return (
+    <div>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList
+        title='Latest Books'
+        books={sampleBooks}
+        containerClassName='mt-28'
+      />
+    </div>
+  );
+};
 
 export default Home;
